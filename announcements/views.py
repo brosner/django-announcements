848d37b56854c7817000be206067a66a1cc26c9f
Brian Rosner <brosner@gmail.com>
2008-05-24 18:22:21 +0000

    Added site-wide announcements.
    git-svn-id: https://django-announcements.googlecode.com/svn/trunk@9 4e50ab13-fc4d-0410-b010-e1608ea6a288

--- announcements/views.py
+++ announcements/views.py
@@ -7,11 +7,7 @@ from announcements.models import Announcement
 
 
 def announcement_list(request):
-    announcements_hidden = request.session.get("announcements_hidden", set())
-    queryset = Announcement.objects.exclude(pk__in=announcements_hidden)
-    if not request.user.is_authenticated():
-        queryset = queryset.filter(members_only=False)
-    queryset = queryset.order_by("-creation_date")
+    queryset = Announcement.objects.current(request)
     return list_detail.object_list(request, **{
         "queryset": queryset,
         "allow_empty": True,

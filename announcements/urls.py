c5a1704c9912c1df3f1425a6c796431a67321a04
Brian Rosner <brosner@gmail.com>
2008-05-24 17:31:21 +0000

    Fixed some typos. Added the ability to hide announcements on a per request basis.
    git-svn-id: https://django-announcements.googlecode.com/svn/trunk@6 4e50ab13-fc4d-0410-b010-e1608ea6a288

--- announcements/urls.py
+++ announcements/urls.py
@@ -3,12 +3,8 @@ from django.conf.urls.defaults import *
 from django.views.generic import list_detail
 
 from announcements.models import Announcement
+from announcements.views import *
 
-
-announcement_list_info = {
-    "queryset": Announcement.objects.order_by("-creation_date"),
-    "allow_empty": True,
-}
 
 announcement_detail_info = {
     "queryset": Announcement.objects.all(),
@@ -17,5 +13,7 @@ announcement_detail_info = {
 urlpatterns = patterns("",
     url(r"^(?P<object_id>\d+)/$", list_detail.object_detail,
         announcement_detail_info, name="announcement_detail"),
-    url(r"^$", list_detail.object_list, announcement_list_info),
+    url(r"^(?P<object_id>\d+)/hide/$", announcement_hide,
+        name="announcement_hide"),
+    url(r"^$", announcement_list, name="announcement_home"),
 )

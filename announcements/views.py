d739c4499a7ada520f5bd7d4912ded63e89e0a3f
brosner <brosner>
2008-05-24 18:30:30 +0000

    Perform a redirect to the next GET variable.

--- announcements/views.py
+++ announcements/views.py
@@ -1,5 +1,5 @@
 
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.views.generic import list_detail
 from django.shortcuts import get_object_or_404
 
@@ -18,7 +18,9 @@ def announcement_hide(request, object_id):
     Mark this announcement hidden in the session for the user.
     """
     announcement = get_object_or_404(Announcement, pk=object_id)
+    # TODO: perform some basic security checks here to ensure next is not bad
+    redirect_to = request.GET.get("next")
     announcements_hidden = request.session.get("announcements_hidden", set())
     announcements_hidden.add(announcement.pk)
     request.session["announcements_hidden"] = announcements_hidden
-    return HttpResponse("hiding %s" % object_id)
+    return HttpResponseRedirect(redirect_to)

2b7a677abaa1a2d821d0a312aeb05d5e14fa41d0
brosner <brosner>
2008-05-24 18:48:33 +0000

    Added a default value to creation_date in Announcement to be the current datetime.

--- announcements/models.py
+++ announcements/models.py
@@ -1,4 +1,6 @@
 
+from datetime import datetime
+
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
@@ -26,7 +28,7 @@ class Announcement(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
     creator = models.ForeignKey(User)
-    creation_date = models.DateTimeField()
+    creation_date = models.DateTimeField(default=datetime.now)
     site_wide = models.BooleanField()
     members_only = models.BooleanField()
     

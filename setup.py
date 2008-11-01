143239dc108d3425fd0c391532dddde2c2f78a0f
Brian Rosner <brosner@gmail.com>
2008-11-01 01:23:02 +0000

    Added a setup.py file.
    git-svn-id: https://django-announcements.googlecode.com/svn/trunk@39 4e50ab13-fc4d-0410-b010-e1608ea6a288

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "django-announcements",
+    version = "0.5dev",
+    packages = find_packages(),
+    author = "Brian Rosner",
+    author_email = "brosner@gmail.com",
+    description = "Announcements for your Django powered website.",
+    license = "MIT",
+    url = "http://code.google.com/p/django-announcements",
+)

8c8496f2c241cdbb36f82548dd680a9072b8a362
Brian Rosner <brosner@gmail.com>
2009-01-05 02:41:39 +0000

    Hooked up the README file into long_description.
    git-svn-id: https://django-announcements.googlecode.com/svn/trunk@48 4e50ab13-fc4d-0410-b010-e1608ea6a288

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     author = "Brian Rosner",
     author_email = "brosner@gmail.com",
     description = "Announcements for your Django powered website.",
+    long_description = open("README").read(),
     license = "MIT",
     url = "http://code.google.com/p/django-announcements",
     packages = [

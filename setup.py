f5115bb5bc34f4340578b73ece4304eab84169d2
Brian Rosner <brosner@gmail.com>
2009-01-05 02:27:49 +0000

    Switch to using distutils from setuptools.
    git-svn-id: https://django-announcements.googlecode.com/svn/trunk@43 4e50ab13-fc4d-0410-b010-e1608ea6a288

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 
-from setuptools import setup, find_packages
+from distutils.core import setup
 
 setup(
     name = "django-announcements",
     version = "0.5dev",
-    packages = find_packages(),
     author = "Brian Rosner",
     author_email = "brosner@gmail.com",
     description = "Announcements for your Django powered website.",
     license = "MIT",
     url = "http://code.google.com/p/django-announcements",
+    packages = [
+        "announcements",
+    ],
 )
